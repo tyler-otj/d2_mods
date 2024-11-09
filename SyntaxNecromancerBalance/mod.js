6c82f709501302b9f48159e82572ad4bb6e7e9bc
k3m1c <80085563+k3m1c@users.noreply.github.com>
2024-11-09 16:01:29 -0700

    Update mod.js
    -Increased Velocity and Runspeed of Skeletons/Mages to 25
    
    -Added additional Bone Spear projectile per 10 levels

--- SyntaxNecromancerBalance/mod.js
+++ SyntaxNecromancerBalance/mod.js
@@ -3,10 +3,40 @@
 	const skills = D2RMM.readTsv(skillsFilename);
 
 	skills.rows.forEach(row => {
+		
+		//Skeleton Mastery: HP/LVL 8 -> 12
 		if(row["skill"] == "Skeleton Mastery"){
-			row["Param1"] = 12; //bump hp/level from 8 -> 12
-		}else if(row["skill"] == "Golem Mastery"){
-			row["Param2"] = 30; //bump hp%/level from 20 -> 30
+			row["Param1"] = 12; 
+		}
+		
+		//Golem Mastery: HP%/LVL 20 -> 30
+		if(row["skill"] == "Golem Mastery"){
+			row["Param2"] = 30; 
+		}
+		
+		//Bonespear: Extra BS per 10 lvls & Synergy boost
+		if(row["skill"] == "Bone Spear"){
+			row["srvdofunc"] = 8;
+			row["cltdofunc"] = 17;
+			row["srvmissilea"] = "bonespear";
+			row["cltmissilea"] = "bonespear";
+			row["cltmissileb"] = "bonespear";
+			row["calc1"] = "(skill('Bone Spear'.lvl) / 10) + 1";
+			// Needed for the number of missiles skill description
+			row["calc6"] = "skill('Bone Spear'.lvl) - skill('Bone Spear'.blvl)";
+			row["Param8"] = 11;
+		}
+
+		// Bone Spirit More Synergy Bonus
+		if(row["skill"] == "Bone Spirit"){	
+			row["Param8"] = 11;
+		}
+
+		// Teeth: More Synergy Bonus
+		if(row["skill"] === "Teeth") {
+			row["Param8"] = 20;	  
+			// Use less mana: 2^6/256*6 = 
+			row["manashift"] = "6";
 		}
 	});
 
@@ -26,3 +56,4 @@
 
 	D2RMM.writeTsv(monstatsFilename, monstats);
 }
+
